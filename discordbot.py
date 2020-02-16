24cb3c36471586a26bd930f6b4fd76327e37fece
frch7 <60838994+frch7@users.noreply.github.com>
2020-02-16 19:20:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -31,8 +31,8 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
+    # 「.neko」と発言したら「にゃーん」が返る処理
+    if message.content == '.neko':
         await message.channel.send('にゃーん')
 
 # Botの起動とDiscordサーバーへの接続
