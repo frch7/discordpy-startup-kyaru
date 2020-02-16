8b37f2626fb2e24ce1028649ba4a2add8bb8783f
frch7 <60838994+frch7@users.noreply.github.com>
2020-02-16 22:38:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -35,8 +35,8 @@ async def on_message(message):
     if message.author == client.user:
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    if message.content.startswith('@neko'):
+        await message.channel.send('にゃーん♪')
 
 
 # Botの起動とDiscordサーバーへの接続
