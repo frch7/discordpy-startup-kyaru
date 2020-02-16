452ddc6ca0ae20d25ab6cafe0400f3cc94a7fbda
frch7 <60838994+frch7@users.noreply.github.com>
2020-02-16 22:19:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,6 +14,7 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='.')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
 # エラー発生時に動作する処理 (頭文字「.」のコマンド)
 @bot.event
 async def on_command_error(ctx, error):
@@ -26,15 +27,17 @@ async def on_command_error(ctx, error):
 async def ヤバい(ctx):
     await ctx.send('ヤバいわよ！')
 
+
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「@neko」と発言したら「にゃーん」が返る処理
-    if message.content == '@neko':
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
         await message.channel.send('にゃーん')
+
 
 # Botの起動とDiscordサーバーへの接続
 bot.run(token)
