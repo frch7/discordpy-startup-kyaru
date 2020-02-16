303b903f8270228f61e70c385730bdc850acdee4
frch7 <60838994+frch7@users.noreply.github.com>
2020-02-17 01:42:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,45 +1,26 @@
-# インストールした discord.py を読み込む
 # Bot Commands Frameworkを使えるようにする
 # OSに依存した機能を使えるようにする
 # スタックトレース (エラー情報) を使えるようにする
-import discord
 from discord.ext import commands
 import os
 import traceback
 
-# 接続に必要なオブジェクトを生成する
 # Bot Commands Frameworkで頭文字「.」のコマンドを使えるようにする
 # 自分のBotのアクセストークンを設定する
-client = discord.Client()
 bot = commands.Bot(command_prefix='.')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-# エラー発生時に動作する処理 (頭文字「.」のコマンド)
+# エラー発生時に動作する処理
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-# メッセージ受信時に動作する処理 (頭文字「.」のコマンド)
-@bot.command()
-async def ヤバい(ctx):
-    await ctx.send('ヤバいわよ！')
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
 # メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
+@bot.command()
+async def 頼んだ(ctx):
+    await ctx.send('https://stickershop.line-scdn.net/stickershop/v1/sticker/60968934/android/sticker.png;compress=true')
 
 # Botの起動とDiscordサーバーへの接続
 bot.run(token)
